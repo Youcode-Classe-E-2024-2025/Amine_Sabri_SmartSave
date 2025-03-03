9cd5fa69bc48fb7ab8aaa3d9a5f3c72330552025
sabri-amine <sabriamine368@gmail.com>
2025-03-03 10:08:56 +0000

    j'ai ajouté filleable et reation avec prodile

--- savesmart/app/Models/SavingsGoal.php
+++ savesmart/app/Models/SavingsGoal.php
@@ -2,9 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class SavingsGoal extends Model
 {
-    //
+    use HasFactory;
+    protected $fillable = ['profile_id', 'name', 'target_amount', 'saved_amount', 'deadline'];
+    public function profile() {
+        return $this->belongsTo(Profile::class);
+    }
 }
