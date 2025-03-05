282bd48f5f9b8e77423324fdd59111e48666ec8a
sabri-amine <sabriamine368@gmail.com>
2025-03-05 15:38:24 +0000

    j'ai modifier route /login à  /

--- savesmart/routes/web.php
+++ savesmart/routes/web.php
@@ -14,7 +14,7 @@ use App\Http\Controllers\SavingsGoalController;
 Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register');
 Route::post('/register', [AuthController::class, 'register']);
 
-Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::get('/', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout')->middleware('auth');
