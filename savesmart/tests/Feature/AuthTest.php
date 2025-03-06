bf84131ae91e49a0afa8c2dd61096380a0ac97f7
sabri-amine <sabriamine368@gmail.com>
2025-03-06 11:08:15 +0000

    j'ai ajouté test unit dans authController

--- savesmart/tests/Feature/AuthTest.php
+++ savesmart/tests/Feature/AuthTest.php
@@ -1,20 +1,41 @@
 <?php
 
-namespace Tests\Feature;
+namespace Tests\Unit;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
 
 class AuthTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+
+    public function test_user_can_login()
+    {
+        // Créer un utilisateur pour se connecter
+        $user = User::factory()->create([
+            'password' => Hash::make('password123'),
+        ]);
+
+        $response = $this->post('/login', [
+            'email' => $user->email,
+            'password' => 'password123',
+        ]);
+
+        $response->assertRedirect(route('profiles.index'));
+        $response->assertSessionHas('success', 'Bienvenue Admin !');
+    }
+
+    public function test_login_fails_with_invalid_credentials()
+    {
+        $response = $this->post('/login', [
+            'email' => 'wronguser@example.com',
+            'password' => 'wrongpassword',
+        ]);
+
+        $response->assertSessionHasErrors('email');
     }
 }
